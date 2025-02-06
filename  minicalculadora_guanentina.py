59b91acb97f2fcd00379e2ec52f77241bb8fc523
juanjo0824 <juandelgado1@colegioguanenta.edu.co>
2025-02-06 16:37:39 -0500

    actualizacion

---
+++  minicalculadora_guanentina.py
@@ -0,0 +1,35 @@
+#### Output
+print("Calculadora.py")
+print("Operadores")
+print("==========================================")
+y = int(input("Introducir el primer numero: "))
+print("==========================================")
+x = int(input("Introducir el segundo numero: "))
+print("==========================================")
+
+print("Selecciona una operación:")
+print("1. Sumar")
+print("2. Restar")
+print("3. Multiplicar")
+print("4. Dividir")
+print("5. Potencia")
+
+instruccion = int(input("Con que operador quieres (?) "))
+
+
+if instruccion >= 7:
+    print("Valor no valido.")
+else:
+    print("Puedes Seguir usando el programa. ")
+
+if instruccion == 1:
+    print("El Valor es: ", x + y)
+
+if instruccion == 2:
+    print(x - y)
+if instruccion == 3:
+    print("El valor es: ", y * x)
+if instruccion == 4:
+    print("El valor es: ", y / x)
+if instruccion == 5:
+    print("El valor es: ", y ** x)
